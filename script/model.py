7706f7058bc760f58070c79daf25131939e427ff
Johannes Huber <johannes.huber@trustyou.net>
2018-07-14 22:58:50 +0200

    Code cleanups

--- script/model.py
+++ script/model.py
@@ -1,49 +1,41 @@
 import torch
-from allennlp.modules import ConditionalRandomField
+import torch.nn as tnn
+import torch.nn.functional as F
 
-class Model(torch.nn.Module):
-    def __init__(self, gen_emb, domain_emb, num_classes=3, dropout=0.5, crf=False, lbd=0.0):
+
+class Model(tnn.Module):
+    def __init__(self, gen_emb, domain_emb, num_classes=3, dropout=0.5):
         super(Model, self).__init__()
-        self.gen_embedding = torch.nn.Embedding(gen_emb.shape[0], gen_emb.shape[1])
-        self.gen_embedding.weight=torch.nn.Parameter(torch.from_numpy(gen_emb), requires_grad=False)
-        self.domain_embedding = torch.nn.Embedding(domain_emb.shape[0], domain_emb.shape[1])
-        self.domain_embedding.weight=torch.nn.Parameter(torch.from_numpy(domain_emb), requires_grad=False)
-    
-        self.conv1=torch.nn.Conv1d(gen_emb.shape[1]+domain_emb.shape[1], 128, 5, padding=2 )
-        self.conv2=torch.nn.Conv1d(gen_emb.shape[1]+domain_emb.shape[1], 128, 3, padding=1 )
-        self.dropout=torch.nn.Dropout(dropout)
+        self.gen_embedding = tnn.Embedding(gen_emb.shape[0], gen_emb.shape[1])
+        self.gen_embedding.weight = tnn.Parameter(torch.from_numpy(gen_emb), requires_grad=False)
+        self.domain_embedding = tnn.Embedding(domain_emb.shape[0], domain_emb.shape[1])
+        self.domain_embedding.weight = tnn.Parameter(torch.from_numpy(domain_emb), requires_grad=False)
 
-        self.conv3=torch.nn.Conv1d(256, 256, 5, padding=2)
-        self.conv4=torch.nn.Conv1d(256, 256, 5, padding=2)
-        self.conv5=torch.nn.Conv1d(256, 256, 5, padding=2)
-        self.linear_ae=torch.nn.Linear(256, num_classes)
-        self.crf_flag=crf
-        if self.crf_flag:
-            self.crf=ConditionalRandomField(num_classes)            
-          
+        self.conv1 = tnn.Conv1d(gen_emb.shape[1]+domain_emb.shape[1], 128, 5, padding=2)
+        self.conv2 = tnn.Conv1d(gen_emb.shape[1]+domain_emb.shape[1], 128, 3, padding=1)
+        self.dropout = tnn.Dropout(dropout)
+
+        self.conv3 = tnn.Conv1d(256, 256, 5, padding=2)
+        self.conv4 = tnn.Conv1d(256, 256, 5, padding=2)
+        self.conv5 = tnn.Conv1d(256, 256, 5, padding=2)
+        self.linear_ae = tnn.Linear(256, num_classes)
+
     def forward(self, x, x_len, x_mask, x_tag=None, testing=False):
-        x_emb=torch.cat((self.gen_embedding(x), self.domain_embedding(x) ), dim=2)
-        x_emb=self.dropout(x_emb).transpose(1, 2)
-        x_conv=torch.nn.functional.relu(torch.cat((self.conv1(x_emb), self.conv2(x_emb)), dim=1) )
-        x_conv=self.dropout(x_conv)
-        x_conv=torch.nn.functional.relu(self.conv3(x_conv) )
-        x_conv=self.dropout(x_conv)
-        x_conv=torch.nn.functional.relu(self.conv4(x_conv) )
-        x_conv=self.dropout(x_conv)
-        x_conv=torch.nn.functional.relu(self.conv5(x_conv) )
-        x_conv=x_conv.transpose(1, 2)
-        #x_conv=self.dropout(x_conv).transpose(1, 2)
-        x_logit=self.linear_ae(x_conv)
+        x_emb = torch.cat((self.gen_embedding(x), self.domain_embedding(x)), dim=2)
+        x_emb = self.dropout(x_emb).transpose(1, 2)
+        x_conv = F.relu(torch.cat((self.conv1(x_emb), self.conv2(x_emb)), dim=1))
+        x_conv = self.dropout(x_conv)
+        x_conv = F.relu(self.conv3(x_conv))
+        x_conv = self.dropout(x_conv)
+        x_conv = F.relu(self.conv4(x_conv))
+        x_conv = self.dropout(x_conv)
+        x_conv = F.relu(self.conv5(x_conv))
+        x_conv = x_conv.transpose(1, 2)
+        x_logit = self.linear_ae(x_conv)
         if testing:
-            if self.crf_flag:
-                score=self.crf.viterbi_tags(x_logit, x_mask)
-            else:
-                x_logit=x_logit.transpose(2, 0)
-                score=torch.nn.functional.log_softmax(x_logit).transpose(2, 0)
+            x_logit = x_logit.transpose(2, 0)
+            score = F.log_softmax(x_logit).transpose(2, 0)
         else:
-            if self.crf_flag:
-                score=-self.crf(x_logit, x_tag, x_mask)
-            else:
-                x_logit=torch.nn.utils.rnn.pack_padded_sequence(x_logit, x_len, batch_first=True)
-                score=torch.nn.functional.nll_loss(torch.nn.functional.log_softmax(x_logit.data), x_tag.data)
+            x_logit = tnn.utils.rnn.pack_padded_sequence(x_logit, x_len, batch_first=True)
+            score = F.nll_loss(F.log_softmax(x_logit.data), x_tag.data)
         return score
--- script/prep_embedding.py
+++ script/prep_embedding.py
@@ -1,25 +1,26 @@
 import argparse
 import numpy as np
 import json
-import subprocess
+
 
 def gen_np_embedding(fn, word_idx_fn, out_fn, dim=300):
     with open(word_idx_fn) as f:
-        word_idx=json.load(f)
-    embedding=np.zeros((len(word_idx)+2, dim) )
+        word_idx = json.load(f)
+    embedding = np.zeros((len(word_idx)+2, dim))
     with open(fn) as f:
         for l in f:
-            rec=l.rstrip().split(' ')
-            if len(rec)==2: #skip the first line.
-                continue 
+            rec = l.rstrip().split(' ')
+            if len(rec) == 2:  # skip the first line.
+                continue
             if rec[0] in word_idx:
-                embedding[word_idx[rec[0]]]=np.array([float(r) for r in rec[1:] ])
+                embedding[word_idx[rec[0]]] = np.array([float(r) for r in rec[1:]])
     with open(out_fn+".oov.txt", "w") as fw:
         for w in word_idx:
-            if embedding[word_idx[w] ].sum()==0.:
+            if embedding[word_idx[w]].sum() == 0.:
                 fw.write(w+"\n")
-    np.save(out_fn+".npy", embedding.astype('float32') )
-    
+    np.save(out_fn+".npy", embedding.astype('float32'))
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--emb_dir', type=str, default="data/embedding/")
 parser.add_argument('--out_dir', type=str, default="data/prep_data/")
@@ -33,6 +34,7 @@ args = parser.parse_args()
 
 gen_np_embedding(args.emb_dir+args.gen_emb, args.out_dir+args.word_idx, args.out_dir+args.gen_emb, args.gen_dim)
 
-gen_np_embedding(args.emb_dir+args.laptop_emb, args.out_dir+args.word_idx, args.out_dir+args.laptop_emb, args.domain_dim)
+# gen_np_embedding(args.emb_dir+args.laptop_emb, args.out_dir+args.word_idx, args.out_dir+args.laptop_emb, args.domain_dim)
 
-gen_np_embedding(args.emb_dir+args.restaurant_emb, args.out_dir+args.word_idx, args.out_dir+args.restaurant_emb, args.domain_dim)
+gen_np_embedding(args.emb_dir+args.restaurant_emb, args.out_dir+args.word_idx,
+                 args.out_dir+args.restaurant_emb, args.domain_dim)
